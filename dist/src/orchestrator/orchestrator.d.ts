93ca7c46cf44b4e04092c972172c99bf60f16634
Pravin Agale <pravin.agale@accionlabs.com>
2025-10-24 18:09:09 +0530

    Remove dist folder from repository and add to .gitignore

--- dist/src/orchestrator/orchestrator.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Custom error for orchestration failures
- */
-export declare class OrchestrationError extends Error {
-    readonly details?: unknown | undefined;
-    constructor(message: string, details?: unknown | undefined);
-}
-//# sourceMappingURL=orchestrator.d.ts.map
